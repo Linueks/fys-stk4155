b31ed68aa5bfaac2697ea5bb121b2dc6ee46da81
Linueks <42612359+Linueks@users.noreply.github.com>
2021-04-14 08:51:07 +0200

    Coursework in FYSSTK4155

---
+++ Project2/data_handling.py
@@ -0,0 +1,163 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as skl
+import pandas as pd
+import seaborn as sbr
+import argparse
+
+def read_data(filtered=True):
+    """
+    column names:
+
+    'LIMIT_BAL', 'SEX', 'EDUCATION', 'MARRIAGE', 'AGE', 'PAY_0', 'PAY_2',
+    'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6', 'BILL_AMT1', 'BILL_AMT2',
+    'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6', 'PAY_AMT1',
+    'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6',
+    'defaultPaymentNextMonth'
+    """
+
+
+    nanDict = {}
+    if filtered:
+        df = pd.read_excel('credit_card_remove_correlation.xls', header=0, skiprows=0, index_col=0, na_values=nanDict)
+    else:
+        df = pd.read_excel('default_of_credit_card_clients.xls', header=1, skiprows=0, index_col=0, na_values=nanDict)
+        df.rename(index=str, columns={'default payment next month': 'defaultPaymentNextMonth'}, inplace=True)
+
+    return df
+
+
+
+def filter_data(df, filename='filtered_credit_card_data.xls'):
+    """Removes unnecessary / unwanted values from datasets and saves to new file
+
+    Parameters:
+    df (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+    filename (str): filename of file to save to.
+    """
+
+    # Drop unnecessary values
+    df = df.drop(df[(df.MARRIAGE == 0)].index)
+    df = df.drop(df[(df.EDUCATION == 0) &
+                    (df.EDUCATION == 5) &
+                    (df.EDUCATION == 6)].index)
+    
+    df = df.drop('BILL_AMT6')
+    df = df.drop('BILL_AMT5')
+    df = df.drop('BILL_AMT4')
+    df = df.drop('BILL_AMT3')
+    df = df.drop('BILL_AMT2')
+
+    df.to_excel(filename)
+
+
+
+
+def generate_scatter(dataframe, category_1, category_2):
+    """Generates scatter plot of the two categories supplied. Will label axes
+    and supply title based on categories input.
+
+    Parameters:
+    dataframe (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+
+    category_1 (str): First category for comparison
+    category_2 (str): Second category for comparison
+
+
+    """
+
+
+
+    plt.scatter(dataframe[category_1], dataframe[category_2], alpha=0.4, edgecolors='w')
+    plt.xlabel(category_1)
+    plt.ylabel(category_2)
+    plt.title(f'{category_2} against {category_1}')
+    plt.show()
+
+
+
+def generate_jointplot(dataframe, category_1, category_2):
+    """Function using seaborn module for plotting a joint plot of two
+    categories present in credit card dataset
+
+    Parameters:
+    dataframe (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+
+    category_1 (str): First category for comparison
+    category_2 (str): Second category for comparison
+
+
+    """
+
+
+    joint_plot = sbr.jointplot(x=f'{category_1}', y=f'{category_2}',
+                            data=dataframe, kind='kde', space=0,
+                            height=5, ratio=4)
+    plt.show()
+
+
+
+def generate_histogram(dataframe, category):
+    """Function using seaborn module for plotting of histogram for the given
+    category present in the credit card dataset
+
+    Parameters:
+    dataframe (pandas dataframe): dataframe containing dataset of credit card
+                                    default data.
+
+    category (str): First category for comparison
+
+
+    """
+
+
+    histogram = sbr.distplot(dataframe[category])
+    plt.show()
+
+
+
+def generate_correlation_matrix(dataframe):
+    f = plt.figure(figsize=(19, 15))
+    plt.matshow(dataframe.corr(), fignum=f.number)
+    plt.xticks(range(dataframe.shape[1]), dataframe.columns, fontsize=14, rotation=90)
+    plt.yticks(range(dataframe.shape[1]), dataframe.columns, fontsize=14)
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize=14)
+    #plt.tight_layout()
+    plt.show()
+
+def frankes_function(x, y, noise_magnitude):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.  - 0.1*(9*y+1))
+    term3 = 0.50*np.exp(-(9*x-7)**2 / 4.   - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2        - (9*y-7)**2)
+    noise =  noise_magnitude * np.random.randn(len(x), len(x)) 
+
+    return term1 + term2 + term3 + term4
+
+def get_franke_data(N):
+    
+    x, y = np.linspace(0, 1, N), np.linspace(0, 1, N)
+    x, y = np.meshgrid(x, y)
+    z = frankes_function(x, y, 0.05)
+    
+    X = x, y
+    
+    return X, z
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filtered', help='uses filtered credit card data', action='store_true')
+    args = parser.parse_args()
+    
+    dataframe = read_data(filtered=args.filtered)
+
+    
+    generate_correlation_matrix(dataframe)
+    #generate_scatter(dataframe, 'MARRIAGE', 'AGE')
+    #generate_jointplot(dataframe, 'MARRIAGE', 'AGE')
+    #generate_histogram(dataframe, 'AGE')
---
+++ Project2/neural_network.py
@@ -0,0 +1,171 @@
+import numpy as np
+
+np.random.seed(1337)
+
+class NeuralNetwork:
+    
+    def __init__(self, X, y, hidden_layers, hidden_activation_functions, output_activation_function, learning_rate, lambda_):
+        self.X = X
+        self.hidden_layers = hidden_layers
+        self.h_activation_funcs = hidden_activation_functions
+        self.o_activation_func = output_activation_function
+        self.categories = y.shape[1]
+        self.eta = learning_rate
+        self.lmbd = lambda_
+        
+        self.y = y
+        
+        self.initialize()
+           
+    def initialize(self):
+        """
+        Initialize all arrays of values based on the amount of hidden layers, 
+        neurons in each layer, number of inputs and number of categories.
+        """
+        self.a = [0 for i in range(len(self.hidden_layers) + 2)]
+        self.z = [0 for i in range(len(self.hidden_layers) + 1)]
+        self.w = [0 for i in range(len(self.hidden_layers) + 1)]
+        self.b = [0 for i in range(len(self.hidden_layers) + 1)]
+    
+        #Data in input layer
+        self.a[0] = self.X
+        
+        #Weights between input layer and first hidden layer
+        self.w[0] = np.random.uniform(size=(self.X.shape[1], self.hidden_layers[0]))
+        
+        #Hidden layers
+        for l, neurons in enumerate(self.hidden_layers):
+            self.a[l+1] = np.zeros(neurons)
+            self.z[l+1] = np.zeros(neurons)
+            self.b[l] = np.zeros(neurons) + 0.01
+            
+            if l > 0:
+                self.w[l] = np.random.uniform(-1, 1, size=(self.hidden_layers[l-1], neurons))
+                
+        #Last layer
+        self.a[-1] = np.zeros(self.categories)
+        self.z[-1] = np.zeros(self.categories)
+        self.b[-1] = np.zeros(self.categories) + 0.01
+        self.w[-1] = np.random.uniform(-1, 1, size=(self.hidden_layers[-1], self.categories))
+        
+    def feed_forward(self):
+        """
+        Feed forward through the network, starting at the first layer and
+        applying the current biases and weights along the way
+        """
+        #Feed forward through each hidden layer
+        for l in range(1, len(self.hidden_layers) + 1):
+            self.z[l] = np.matmul(self.a[l-1], self.w[l-1]) + self.b[l-1]
+            self.a[l] = self.activation(self.z[l], self.h_activation_funcs[l-1])
+            
+        #Last layer
+        self.z[-1] = np.matmul(self.a[-2], self.w[-1]) + self.b[-1]
+        self.a[-1] = self.activation(self.z[-1], self.o_activation_func)
+        
+    def gradients(self, layer_error, l):
+        """
+        Function for calculating the gradients for the biases and weights,
+        given the layer_error and current layer
+        """
+        b_gradient = np.sum(layer_error, axis=0)
+        w_gradient = np.matmul(self.a[l].T, layer_error)
+        if self.lmbd > 0:
+            w_gradient += self.lmbd * self.w[l]
+        
+        return b_gradient, w_gradient
+        
+    def back_propagation(self):
+        """
+        Use gradient descent to tune the biases and weights
+        """
+        #Output layer
+        layer_error = self.a[-1] - self.y
+        
+        b_gradient, w_gradient = self.gradients(layer_error, len(self.hidden_layers))
+
+        self.b[-1] -= self.eta * b_gradient / len(self.X)
+        self.w[-1] -= self.eta * w_gradient / len(self.X)
+        
+        #Loop backwards through rest of layers
+        for l in range(len(self.hidden_layers), 0, -1):
+            layer_error = np.matmul(layer_error, self.w[l].T) * self.activation_derivative(self.a[l], self.h_activation_funcs[l-1])
+            b_gradient, w_gradient = self.gradients(layer_error, l-1)
+            self.b[l-1] -= self.eta * b_gradient / len(self.X)
+            self.w[l-1] -= self.eta * w_gradient / len(self.X)
+            
+    def train(self, epochs):
+        """
+        Feed forward and back propaagate the specified number of times.
+        Batch splitting for stochastic gradient descent would also happen here,
+        however it is not included in this class. see old_neural_network.py
+        """
+        for i in range(epochs):
+            self.feed_forward()
+            self.back_propagation()
+        
+    def predict(self, X):
+        """
+        Use the current weights and biases to make a prediction on the dataset X
+        by inserting into the first layer and feeding forward
+        """
+        self.a[0] = X
+        self.feed_forward()
+        
+        y_pred = np.argmax(self.a[-1], axis=1)
+        
+        return y_pred
+    
+    def predict_regression(self, X):
+        """
+        
+        """
+        self.a[0] = X
+        self.feed_forward()
+        return self.a[-1]
+    
+    def activation(self, x, method):
+        """
+        Activation functions
+        """
+        if (method == 'sigmoid'):
+            t = 1./(1 + np.exp(-x))
+
+        elif (method == 'softmax'):
+            if len(x.shape) > 1:
+                exp_term = np.exp(x)
+                t = exp_term / np.sum(exp_term, axis=1, keepdims=True)
+            else:
+                exp_term = np.exp(x)
+                t = exp_term / np.sum(exp_term)
+
+        elif (method == 'tanh'):
+            t = np.tanh(x)
+
+        elif (method == 'relu'):
+            neg = np.where(x < 0)
+            x[neg] = 0
+            t = x
+        else:
+            t = x
+
+        return t
+            
+    def activation_derivative(self, x, method):
+        """
+        Derivative of activation functions
+        """
+        if method == 'sigmoid':
+            return x*(1 - x)
+
+        elif method == 'tanh':
+            return 1 - x**2
+
+        elif method == 'relu':
+            pos = np.where(x > 0)
+            neg = np.where(x <= 0)
+            x[pos] = 1
+            x[neg] = 0
+            return x
+
+        else:
+            return 1
